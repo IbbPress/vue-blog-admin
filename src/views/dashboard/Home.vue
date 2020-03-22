2c57416c5ef28c6727da73fc22b168aac4223fd4
wencaizhang <1052642137@qq.com>
2020-03-22 14:27:00 +0800

    删去没有用到的资源引用

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -65,7 +65,7 @@ import { mapState } from 'vuex'
 
 import { PageView } from '@/layouts'
 import HeadInfo from '@/components/tools/HeadInfo'
-import { Radar } from '@/components'
+// import { Radar } from '@/components'
 
 import DraftVue from './modules/Draft.vue'
 import NavVue from './modules/Nav.vue'
@@ -73,14 +73,14 @@ import ActivityVue from './modules/Activity.vue'
 import ProjectsVue from './modules/Projects.vue'
 // import { getRoleList, getServiceList } from '@/api/manage'
 
-const DataSet = require('@antv/data-set')
+// const DataSet = require('@antv/data-set')
 
 export default {
   name: 'Workplace',
   components: {
     PageView,
     HeadInfo,
-    Radar,
+    // Radar,
     NavVue,
     DraftVue,
     ActivityVue,
@@ -97,51 +97,7 @@ export default {
       },
       rightCol: {
         xl: 12, lg: 12, md: 24, sm: 24, xs: 24
-      },
-
-      projects: [],
-      loading: true,
-      radarLoading: true,
-      activities: [],
-      teams: [],
-
-      // data
-      axis1Opts: {
-        dataKey: 'item',
-        line: null,
-        tickLine: null,
-        grid: {
-          lineStyle: {
-            lineDash: null
-          },
-          hideFirstLine: false
-        }
-      },
-      axis2Opts: {
-        dataKey: 'score',
-        line: null,
-        tickLine: null,
-        grid: {
-          type: 'polygon',
-          lineStyle: {
-            lineDash: null
-          }
-        }
-      },
-      scale: [{
-        dataKey: 'score',
-        min: 0,
-        max: 80
-      }],
-      axisData: [
-        { item: '引用', a: 70, b: 30, c: 40 },
-        { item: '口碑', a: 60, b: 70, c: 40 },
-        { item: '产量', a: 50, b: 60, c: 40 },
-        { item: '贡献', a: 40, b: 50, c: 40 },
-        { item: '热度', a: 60, b: 70, c: 40 },
-        { item: '引用', a: 70, b: 50, c: 40 }
-      ],
-      radarData: []
+      }
     }
   },
   computed: {
@@ -156,59 +112,9 @@ export default {
   created () {
     this.user = this.userInfo
     this.avatar = this.userInfo.avatar
-
-    // getRoleList().then(res => {
-    //   // console.log('workplace -> call getRoleList()', res)
-    // })
-
-    // getServiceList().then(res => {
-    //   // console.log('workplace -> call getServiceList()', res)
-    // })
   },
-  mounted () {
-    // this.getProjects()
-    // this.getActivity()
-    // this.getTeams()
-    // this.initRadar()
-  },
-  methods: {
-    getProjects () {
-      this.$http.get('/list/search/projects')
-        .then(res => {
-          this.projects = res.result && res.result.data
-          this.loading = false
-        })
-    },
-    getActivity () {
-      this.$http.get('/workplace/activity')
-        .then(res => {
-          this.activities = res.result
-        })
-    },
-    getTeams () {
-      this.$http.get('/workplace/teams')
-        .then(res => {
-          this.teams = res.result
-        })
-    },
-    initRadar () {
-      this.radarLoading = true
-
-      this.$http.get('/workplace/radar')
-        .then(res => {
-          const dv = new DataSet.View().source(res.result)
-          dv.transform({
-            type: 'fold',
-            fields: ['个人', '团队', '部门'],
-            key: 'user',
-            value: 'score'
-          })
-
-          this.radarData = dv.rows
-          this.radarLoading = false
-        })
-    }
-  }
+  mounted () {},
+  methods: {}
 }
 </script>
 
